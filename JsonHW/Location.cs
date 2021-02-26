f774ba238575631c89281f7880606f711732eb1e
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-26 21:02:47 +0600

    Добавьте файлы проекта.

---
+++ JsonHW/Location.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonHW
+{
+    public class Location
+    {
+        public int request { get; set; }
+        public int type { get; set; }
+        public string query { get; set; }
+        public string language { get; set; }
+        public int unit { get; set; }
+        public int location { get; set; }
+        public string name { get; set; }
+        public string Name { get; internal set; }
+        public int country { get; set; }
+        public string Country { get; internal set; }
+        public int region { get; set; }
+        public string Region { get; internal set; }
+        public string lat { get; set; }
+        public double Lat { get; internal set; }
+        public string lon { get; set; }
+        public double Lon { get; internal set; }
+        public int timezone_id { get; set; }
+        public string Timezone_id { get; internal set; }
+        public int localtime { get; set; }
+        public int localtime_epoch { get; set; }
+        public string utc_offset { get; set; }
+        public int current { get; set; }
+        public int observation_time { get; set; }
+        public int temperature { get; set; }
+        public string weather_code { get; set; }
+        public string weather_icons { get; set; }
+        public string weather_descriptions { get; set; }
+        public int wind_speed { get; set; }
+        public int wind_degree { get; set; }
+        public string wind_dir { get; set; }
+        public int pressure { get; set; }
+        public int precip { get; set; }
+        public int humidity { get; set; }
+        public int cloudcover { get; set; }
+        public int feelslike { get; set; }
+        public int uv_index { get; set; }
+        public int visibility { get; set; }
+        public string is_day { get; set; }
+        public int Locationtime { get; internal set; }
+    }
+}
---
+++ JsonHW/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core.Composing;
+
+namespace JsonHW
+{
+    class Program
+    {
+        public static int Localtime_epoch { get; private set; }
+        public static double Utc_offset { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var requestList = new List<Request>
+            {
+                new Request
+                {
+                    Type = "City",
+                    Query = "Astana, Kazakhstan",
+                    Language = "en",
+                    Unit = "m"
+                },
+                new Location
+                {
+                    Name = "Astana",
+                    Country = "Kazakhstan",
+                    Region = "Astana",
+                    Lat = 51.181,
+                    Lon = 71.428,
+                    Timezone_id = "Asia\/Almaty",
+                    Locationtime = 2021-02-15 21:04,
+                    Localtime_epoch = 1613423040,
+                    Utc_offset = 6.0
+                },
+                new Current 
+                {
+                    Observation_time = 03:04 PM,
+                    Temperature = -6,
+                    Weather_code = 116,
+                    Weather_icons = "https:\/\/assets.weatherstack.com\/images\/wsymbols01_png_64\/wsymbol_0004_black_low_cloud.png",
+                    Weather_descriptions = "Partly cloudy",
+                    Wind_speed = 19,
+                    Wind_degree = 190,
+                    Wind_dir = "S",
+                    Pressure = 1018,
+                    Precip = 0,
+                    Humidity = 93,
+                    Cloudcover = 50,
+                    Feelslike = -12,
+                    Uv_index = 1,
+                    Visibility = 10,
+                    Is_day = "no"
+                }
+            };
+        }
+    }
+}
